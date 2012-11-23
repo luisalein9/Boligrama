56b9693da20c5b6b6154a5f94636802942d52754
root <root@osvaldo172.(none)>
2012-11-22 21:12:39 -0600

    actualizado

---
+++ application/controllers/divisiones_c.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Divisiones_c extends CI_Controller {
+	 
+	function __construct(){
+        parent::__construct();
+        $this->load->helper(array('html', 'url'));
+        //$this->load->model('divisiones_m'); // Load the model
+    }
+
+
+	public function index()
+	{
+		$this->load->view('divisiones_v');	
+	}	
+}	
---
+++ application/views/divisiones_v.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width" />
+		<link rel="stylesheet" href="<?=base_url(); ?>statics/foundation/stylesheets/foundation.min.css">
+		<script src="<?=base_url(); ?>statics/foundation/javascripts/foundation.min.js"></script>
+        <script src="<?=base_url(); ?>statics/foundation/javascripts/modernizr.foundation.js"></script>
+		<script src="<?=base_url(); ?>statics/foundation/javascripts/marketing_docs.js"></script>
+		<script src="<?=base_url(); ?>statics/js/jquery-1.8.2.js"></script>
+   </head>
+   
+   <body>
+   		<title>Boligrama-Divisiones</title>
+   		<div class="continer">
+   				<h1>hola</h1>
+   		</div>
+   	
+   </body>
+</html>
