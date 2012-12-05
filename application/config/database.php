f9e91533178f96f7ce6baeedc9bc03afaf5d61df
Precise <precise@Gate.(none)>
2012-12-04 19:03:47 -0600

    Actores.js

--- application/config/database.php
+++ application/config/database.php
@@ -50,18 +50,11 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-<<<<<<< HEAD
-//$db['default']['password'] = '207341483';
-//contraseña fac
-$db['default']['password'] = '3034facundo';
-$db['default']['database'] = 'mydb';
-=======
-$db['default']['password'] = '207341483';
+//$db['default']['password'] = '3034facundo';
 //$db['default']['password'] = 'root';
 //$db['default']['password'] = '207341483';
 $db['default']['password'] = 'Luisalein9*';
 $db['default']['database'] = 'boligramaDB';
->>>>>>> 14de3d826e082671ec722a625937e169ec46b176
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
